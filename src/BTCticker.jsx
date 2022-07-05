b7557697eb49be5c83c0f1ffd197cf41039ccdad
David Garcia <dave74@gmail.com>
2022-07-05 10:03:34 +0200

    adds websocket connection

--- src/BTCticker.jsx
+++ src/BTCticker.jsx
@@ -1,40 +1,57 @@
 import { useEffect, useState } from "react"
-import { domain } from "./domain"
-import btcLogo from "./assets/BitcoinSign.png"
+import { w3cwebsocket as W3CWebSocket } from "websocket"
+import { formattedValue } from "./utils/formattedValue"
 import {
   btcTickerContainer,
   headerClass,
   btcLogoContainer,
-  btcLogoClass,
 } from "./BTCticker.module.css"
 
 function BTCticker() {
-  const [btcPrice, setBtcPrice] = useState()
-  const [fiatCurrency, setFiatCurrency] = useState("eur")
+  const [btcPrice, setBtcPrice] = useState(0)
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  const element = document.documentElement
 
   useEffect(() => {
-    domain
-      .get("getBTCPriceUseCase")
-      .execute({ cryptoCoin: "bitcoin", fiatCurrency })
-      .then((data) => {
-        setBtcPrice(data)
-        console.log("DATA", data)
-      })
-  }, [fiatCurrency])
+    const websocket = new W3CWebSocket("wss://ws.bitstamp.net/")
 
-  const handleChange = (evt) => setFiatCurrency(evt.target.value)
+    websocket.onopen = () => {
+      console.log("WebSocket Client Connected")
+      websocket.send(
+        JSON.stringify({
+          event: "bts:subscribe",
+          data: {
+            channel: "live_trades_btceur",
+          },
+        })
+      )
+    }
+    websocket.onmessage = (evt) =>
+      setBtcPrice(
+        formattedValue({
+          value: JSON.parse(evt.data).data.price || 0,
+        })
+      )
+  }, [])
+
+  const toggleFullscreen = () => {
+    if (!isFullscreen) {
+      element.requestFullscreen()
+      setIsFullscreen(true)
+    }
+
+    if (isFullscreen) {
+      document.exitFullscreen()
+      setIsFullscreen(false)
+    }
+  }
 
   return (
     <div className={btcTickerContainer}>
       <header className={headerClass}>
-        <div className={btcLogoContainer}>
-          <img src={btcLogo} className={btcLogoClass} alt="logo" />
-          <h1>{btcPrice?.value}</h1>
+        <div onClick={toggleFullscreen} className={btcLogoContainer}>
+          <h1>{btcPrice}</h1>
         </div>
-        <select onChange={handleChange}>
-          <option value={"eur"}>eur</option>
-          <option value={"usd"}>usd</option>
-        </select>
       </header>
     </div>
   )
