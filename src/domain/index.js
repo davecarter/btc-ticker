f255eb3d2d2f19008049f9f23f452bc70ecfd514
David Garcia <dave74@gmail.com>
2022-07-05 10:02:50 +0200

    remove domain to fecth data

--- src/domain/index.js
+++
@@ -1,9 +0,0 @@
-import { GetBTCPriceUseCase } from "./btc/useCase/GetBTCPriceUseCase"
-
-const useCases = {
-  getBTCPriceUseCase: GetBTCPriceUseCase.create(),
-}
-
-export const domain = {
-  get: (name) => useCases[name],
-}
